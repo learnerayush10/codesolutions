91e8bdcb74af98ae9a3d1c59cf5ffb59da673728
learnerayush10 <106659592+learnerayush10@users.noreply.github.com>
2022-06-02 22:53:34 +0530

    Added solution - LeetHub

---
+++ Subarray with given sum - GFG/subarray-with-given-sum.py
@@ -0,0 +1,49 @@
+#User function Template for python3
+
+#Function to find a continuous sub-array which adds up to a given number.
+class Solution:
+    def subArraySum(self,arr, n, s): 
+       #Write your code here
+       if n == 0:
+           return [-1]
+       l, r = 0, 0
+       csum = 0
+       while r < n:
+           csum += arr[r]
+           r += 1
+           while csum > s and l < (r):
+               csum -= arr[l]
+               l += 1
+           if csum == s:
+               return [l+1, r]
+       return [-1]
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            NS=input().strip().split()
+            N=int(NS[0])
+            S=int(NS[1])
+            
+            A=list(map(int,input().split()))
+            ob=Solution()
+            ans=ob.subArraySum(A, N, S)
+            
+            for i in ans:
+                print(i, end=" ")
+                
+            print()
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
