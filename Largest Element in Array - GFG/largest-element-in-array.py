b86a9563f967dbbde7890cd49cba95f09411e861
learnerayush10 <106659592+learnerayush10@users.noreply.github.com>
2022-07-05 22:19:00 +0530

    Added solution - LeetHub

---
+++ Largest Element in Array - GFG/largest-element-in-array.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+def largest( arr, n):
+    large = 0
+    for i in arr:
+        if large<i:
+            large = i
+    return large
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(largest(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
