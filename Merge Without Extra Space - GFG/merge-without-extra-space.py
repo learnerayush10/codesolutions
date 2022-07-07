ba86162b09467a716a07b6c1e8253826e5de879c
learnerayush10 <106659592+learnerayush10@users.noreply.github.com>
2022-07-07 14:10:57 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.py
+++ Merge Without Extra Space - GFG/merge-without-extra-space.py
@@ -1,28 +1,36 @@
 #User function Template for python3
 
 class Solution:
-    def merge(self, arr1, arr2, n, m): 
-        # code here
-        arr1.extend(arr2)
-        arr1.sort()
-        del arr2[:]
+    
+    #Function to merge the arrays.
+    def merge(self,arr1,arr2,n,m):
+        A = [i for i in arr1]
+        for ele in arr2:
+            A.append(ele)
+        A.sort()
+        ind=0
+        for i in range(n):
+            arr1[i] = A[ind]
+            ind += 1
+        for j in range(m):
+            arr2[j] = A[ind]
+            ind += 1
+        #code here
+    
+
 
 #{ 
 #  Driver Code Starts
-#Initial Template for Python 3
+#Initial template for Python
 
-if __name__ == "__main__": 		
-    tc=int(input())
-    while tc > 0:
-        n, m=map(int, (input().strip().split()))
-        arr1=list(map(int , input().strip().split()))
-        arr2=list(map(int , input().strip().split()))
-        ob = Solution()
-        ans=ob.merge(arr1, arr2, n, m)
-        for x in arr1:
-            print(x, end=" ")
-        for x in arr2:
-            print(x, end=" ")
-        print()
-        tc=tc-1
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        n,m = map(int, input().strip().split())
+        arr1 = list(map(int, input().strip().split()))
+        arr2 = list(map(int, input().strip().split()))
+        ob=Solution()
+        ob.merge(arr1, arr2, n, m)
+        print(*arr1,end=" ")
+        print(*arr2)
 # } Driver Code Ends
